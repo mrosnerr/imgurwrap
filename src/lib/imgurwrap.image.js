fc2554fdbb378f4a7a13509ab797ca6b646784e0
msrxthr <mrosner@outlook.com>
2015-04-13 03:19:11 +0000

    Added image upload/delete logic and tests

--- src/lib/imgurwrap.image.js
+++ src/lib/imgurwrap.image.js
@@ -12,3 +12,49 @@ imgurwrap.getImageData = function(id, callback) {
     }
     return request.get(params, imgurwrapUtil.responseCallback(callback));
 };
+
+imgurwrap.uploadImageURL = function(payload, callback) {
+    var endpoint = imgurwrapUtil.getEndpoint('image');
+    var params = imgurwrapUtil.getBaseRequestParams(endpoint);
+    payload.type = 'URL';
+    params.formData = payload;
+    var err = imgurwrapUtil.validateRequestParams(params);
+    if (err) {
+        return callback(err);
+    }
+    return request.post(params, imgurwrapUtil.responseCallback(callback));
+};
+
+imgurwrap.uploadImageFile = function(payload, callback) {
+    var endpoint = imgurwrapUtil.getEndpoint('image');
+    var params = imgurwrapUtil.getBaseRequestParams(endpoint);
+    payload.type = 'file';
+    params.formData = payload;
+    var err = imgurwrapUtil.validateRequestParams(params);
+    if (err) {
+        return callback(err);
+    }
+    return request.post(params, imgurwrapUtil.responseCallback(callback));
+};
+
+imgurwrap.uploadImageBase64 = function(payload, callback) {
+    var endpoint = imgurwrapUtil.getEndpoint('image');
+    var params = imgurwrapUtil.getBaseRequestParams(endpoint);
+    payload.type = 'base64';
+    params.formData = payload;
+    var err = imgurwrapUtil.validateRequestParams(params);
+    if (err) {
+        return callback(err);
+    }
+    return request.post(params, imgurwrapUtil.responseCallback(callback));
+};
+
+imgurwrap.deleteImage = function(id, callback) {
+    var endpoint = imgurwrapUtil.getEndpoint('image', id);
+    var params = imgurwrapUtil.getBaseRequestParams(endpoint);
+    var err = imgurwrapUtil.validateRequestParams(params);
+    if (err) {
+        return callback(err);
+    }
+    return request.del(params, imgurwrapUtil.responseCallback(callback));
+};
